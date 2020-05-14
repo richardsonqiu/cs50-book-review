3102fd9a44e3228421e4ddd857e39f1a337c712e
Richardson <richardsonqiu@gmail.com>
2020-05-14 11:08:49 +0800

    added database tables

---
+++ tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    isbn VARCHAR UNIQUE NOT NULL,
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    year SMALLINT NOT NULL
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    book_id INTEGER REFERENCES books,
+    rating SMALLINT NOT NULL CONSTRAINT Invalid_Rating CHECK (rating>=1 AND rating <=5),
+    comment VARCHAR
+);
